ad9c80992fd0e8cad29d92adbde48599eb5fd111
hd1801 <hardikdhawan0108@gmail.com>
2021-04-18 21:58:09 +0530

    added nav_drawer

--- app/src/main/java/com/example/tourguideapp/ListActivity.java
+++ app/src/main/java/com/example/tourguideapp/ListActivity.java
@@ -1,27 +1,59 @@
 package com.example.tourguideapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        ArrayList<TouristSpot> List = new ArrayList<>() ;
-        List.add(new TouristSpot(getString(R.string.SampleName),"This i s sample description",R.drawable.sample));
-        List.add(new TouristSpot("Sample Place","This i s sample description",R.drawable.sample));
-        List.add(new TouristSpot("Sample Place","This i s sample description",R.drawable.sample));
-        List.add(new TouristSpot("Sample Place","This i s sample description",R.drawable.sample));
-        ListAdapter adapter = new ListAdapter(this,List);
+
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        ArrayList<TouristSpot> List = new ArrayList<>();
+        List.add(new TouristSpot(getString(R.string.SampleName), "This i s sample description", R.drawable.sample));
+        List.add(new TouristSpot("Sample Place", "This i s sample description", R.drawable.sample));
+        List.add(new TouristSpot("Sample Place", "This i s sample description", R.drawable.sample));
+        List.add(new TouristSpot("Sample Place", "This i s sample description", R.drawable.sample));
+        ListAdapter adapter = new ListAdapter(this, List);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
 
     }
 }
