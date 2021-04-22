9b9482c0b0e9da7a518674b33dc0047216210db4
hd1801 <hardikdhawan0108@gmail.com>
2021-04-23 00:39:11 +0530

    final commit

--- app/src/main/java/com/example/tourguideapp/ListActivity.java
+++ app/src/main/java/com/example/tourguideapp/ListActivity.java
@@ -3,25 +3,31 @@ package com.example.tourguideapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.google.android.material.navigation.NavigationView;
+
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
     private ArrayList<TouristSpot> List;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +46,12 @@ public class ListActivity extends AppCompatActivity {
 
         // to make the Navigation drawer icon always appear on the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+
+        }
 
 
         Bundle bundle = getIntent().getExtras();
@@ -81,26 +93,99 @@ public class ListActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         //TODO:set intents to switch cases transfer data of list, use different bundle keys for passing data (refter main activity)
-        switch (item.getItemId()) {
-            case R.id.nav_hotel:
-            {   
-                break;}
-            case R.id.nav_restaurants:
-                break;
-            case R.id.nav_tourist_spots:
-                break;
-            case R.id.nav_transports:
-                break;
-            case R.id.nav_shops:
-                break;
-            default:
-                break;
-        }
+
         if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.nav_hotel:
+            {
+                ArrayList<TouristSpot> List = new ArrayList<>();
+                List.add(new TouristSpot(getString(R.string.ITC_hotel), getString(R.string.ITC_short_desc), R.drawable.itc,getString(R.string.ITC_desc),getString(R.string.ITC_address),getString(R.string.ITC_contact)));
+                List.add(new TouristSpot(getString(R.string.FourPoints_hotel), getString(R.string.FourPoints_short_desc), R.drawable.fourpoint,getString(R.string.FourPoints_desc),getString(R.string.FourPoints_address),getString(R.string.FourPoints_contact)));
+                List.add(new TouristSpot(getString(R.string.Marriott_hotel), getString(R.string.Marriott_short_desc), R.drawable.marriott,getString(R.string.Marriott_desc),getString(R.string.Marriott_address),getString(R.string.Marriott_contact)));
+                List.add(new TouristSpot(getString(R.string.Le_Méridien_hotel), getString(R.string.Le_Méridien_short_desc), R.drawable.lemeridien,getString(R.string.ITC_desc),getString(R.string.Le_Méridien_address),getString(R.string.Le_Méridien_contact)));
+                Intent i= new Intent(this,ListActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelableArrayList("data",List);
+                i.putExtras(bundle);
+                startActivity(i);
+                break;
+            }
+            case R.id.nav_restaurants:
+            {
+                ArrayList<TouristSpot> List = new ArrayList<>();
+                List.add(new TouristSpot(getString(R.string.RJ14_rest), getString(R.string.RJ14_short_desc), R.drawable.rj14,getString(R.string.RJ14_desc),getString(R.string.RJ14_address),getString(R.string.RJ14_contact)));
+                List.add(new TouristSpot(getString(R.string.The_Rajput_Room_rest), getString(R.string.The_Rajput_Room_short_desc), R.drawable.rajputroom,getString(R.string.The_Rajput_Room_desc),getString(R.string.The_Rajput_Room_address),getString(R.string.The_Rajput_Room_contact)));
+                List.add(new TouristSpot(getString(R.string.Jaipur_Adda_rest), getString(R.string.Jaipur_Adda_short_desc), R.drawable.jaipuradda,getString(R.string.Jaipur_Adda_desc),getString(R.string.Jaipur_Adda_address),getString(R.string.Jaipur_Adda_contact)));
+                List.add(new TouristSpot(getString(R.string.Zolocrust_rest), getString(R.string.Zolocrust_short_desc), R.drawable.zolocrust,getString(R.string.Zolocrust_desc),getString(R.string.Zolocrust_address),getString(R.string.Zolocrust_contact)));
+                List.add(new TouristSpot(getString(R.string.TapriCentral_rest), getString(R.string.TapriCentral_short_desc), R.drawable.tapri,getString(R.string.TapriCentral_desc),getString(R.string.TapriCentral_address),getString(R.string.TapriCentral_contact)));
+                List.add(new TouristSpot(getString(R.string.Chao_Chinese_rest), getString(R.string.Chao_Chinese_short_desc), R.drawable.chao,getString(R.string.Chao_Chinese_desc),getString(R.string.Chao_Chinese_address),getString(R.string.Chao_Chinese_contact)));
+                Intent i= new Intent(this,ListActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelableArrayList("data",List);
+                i.putExtras(bundle);
+                startActivity(i);
+                break;
+            }
+
+            case R.id.nav_shops:
+            {
+                ArrayList<TouristSpot> List = new ArrayList<>();
+                List.add(new TouristSpot(getString(R.string.Chandpole_Bazaar_shop), getString(R.string.Chandpole_Bazaar_short_desc), R.drawable.chandpol,getString(R.string.Chandpole_Bazaar_desc),getString(R.string.Chandpole_Bazaar_address),getString(R.string.Chandpole_Bazaar_contact)));
+                List.add(new TouristSpot(getString(R.string.Tripolia_Bazaar_shop), getString(R.string.Tripolia_Bazaar_short_desc), R.drawable.tripolia,getString(R.string.Tripolia_Bazaar_desc),getString(R.string.Tripolia_Bazaar_address),getString(R.string.Tripolia_Bazaar_contact)));
+                List.add(new TouristSpot(getString(R.string.Johari_Bazaar_shop), getString(R.string.Johari_Bazaar_short_desc), R.drawable.johari,getString(R.string.Johari_Bazaar_desc),getString(R.string.Johari_Bazaar_address),getString(R.string.Johari_Bazaar_contact)));
+                Intent i= new Intent(this,ListActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelableArrayList("data",List);
+                i.putExtras(bundle);
+                startActivity(i);
+                break;
+
+            }
+            case R.id.nav_tourist_spots:{
+                ArrayList<TouristSpot> List = new ArrayList<>();
+                List.add(new TouristSpot(getString(R.string.Birla_Mandir_spot), getString(R.string.Birla_Mandir_short_desc), R.drawable.birlamandir,getString(R.string.Birla_Mandir_desc),getString(R.string.Birla_Mandir_address),getString(R.string.Birla_Mandir_contact)));
+                List.add(new TouristSpot(getString(R.string.Hawa_Mahal_spot), getString(R.string.Hawa_Mahal_short_desc), R.drawable.hawamahal,getString(R.string.Hawa_Mahal_desc),getString(R.string.Hawa_Mahal_address),getString(R.string.Hawa_Mahal_contact)));
+                List.add(new TouristSpot(getString(R.string.Jantar_Mantar_spot), getString(R.string.Jantar_Mantar_short_desc), R.drawable.jantarmantar,getString(R.string.Jantar_Mantar_desc),getString(R.string.Jantar_Mantar_address),getString(R.string.Jantar_Mantar_contact)));
+                List.add(new TouristSpot(getString(R.string.Jal_Mahal_spot), getString(R.string.Jal_Mahal_short_desc), R.drawable.sample,getString(R.string.Jal_Mahal_desc),getString(R.string.Jal_Mahal_address),getString(R.string.Jal_Mahal_contact)));
+                List.add(new TouristSpot(getString(R.string.Galta_Ji_spot), getString(R.string.Galta_Ji_short_desc), R.drawable.galtaji,getString(R.string.Galta_Ji_desc),getString(R.string.Galta_Ji_address),getString(R.string.Galta_Ji_contact)));
+                Intent i= new Intent(this,ListActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelableArrayList("data",List);
+                i.putExtras(bundle);
+                startActivity(i);
+                break;
+            }
+
+            case R.id.nav_transports:{
+                ArrayList<TouristSpot> List = new ArrayList<>();
+                List.add(new TouristSpot(getString(R.string.metro), getString(R.string.metro_short_desc), R.drawable.download,getString(R.string.metro_desc),getString(R.string.metro_address),getString(R.string.metro_contact)));
+                List.add(new TouristSpot(getString(R.string.LowFloor_bus), getString(R.string.LowFloor_short_desc), R.drawable.lowfloor,getString(R.string.metro_desc),getString(R.string.metro_address),getString(R.string.metro_contact)));
+                List.add(new TouristSpot(getString(R.string.Palace_On_Wheels), getString(R.string.Palace_On_Wheels_short_desc), R.drawable.palaceonwheels,getString(R.string.Palace_On_Wheels_desc),getString(R.string.Palace_On_Wheels_address),getString(R.string.Palace_On_Wheels_contact)));
+                Intent i= new Intent(this,ListActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelableArrayList("data",List);
+                i.putExtras(bundle);
+                startActivity(i);
+                break;
+            }
+
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.my_drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
+
+
 
 
--- app/src/main/java/com/example/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/tourguideapp/MainActivity.java
@@ -20,10 +20,10 @@ public class MainActivity extends AppCompatActivity {
     public void openHotelActivity(View view)
     {
         ArrayList<TouristSpot> List = new ArrayList<>();
-        List.add(new TouristSpot(getString(R.string.ITC_hotel), getString(R.string.ITC_short_desc), R.drawable.sample,getString(R.string.ITC_desc),getString(R.string.ITC_address),getString(R.string.ITC_contact)));
-        List.add(new TouristSpot(getString(R.string.FourPoints_hotel), getString(R.string.FourPoints_short_desc), R.drawable.sample,getString(R.string.FourPoints_desc),getString(R.string.FourPoints_address),getString(R.string.FourPoints_contact)));
-        List.add(new TouristSpot(getString(R.string.Marriott_hotel), getString(R.string.Marriott_short_desc), R.drawable.sample,getString(R.string.Marriott_desc),getString(R.string.Marriott_address),getString(R.string.Marriott_contact)));
-        List.add(new TouristSpot(getString(R.string.Le_Méridien_hotel), getString(R.string.Le_Méridien_short_desc), R.drawable.sample,getString(R.string.ITC_desc),getString(R.string.Le_Méridien_address),getString(R.string.Le_Méridien_contact)));
+        List.add(new TouristSpot(getString(R.string.ITC_hotel), getString(R.string.ITC_short_desc), R.drawable.itc,getString(R.string.ITC_desc),getString(R.string.ITC_address),getString(R.string.ITC_contact)));
+        List.add(new TouristSpot(getString(R.string.FourPoints_hotel), getString(R.string.FourPoints_short_desc), R.drawable.fourpoint,getString(R.string.FourPoints_desc),getString(R.string.FourPoints_address),getString(R.string.FourPoints_contact)));
+        List.add(new TouristSpot(getString(R.string.Marriott_hotel), getString(R.string.Marriott_short_desc), R.drawable.marriott,getString(R.string.Marriott_desc),getString(R.string.Marriott_address),getString(R.string.Marriott_contact)));
+        List.add(new TouristSpot(getString(R.string.Le_Méridien_hotel), getString(R.string.Le_Méridien_short_desc), R.drawable.lemeridien,getString(R.string.ITC_desc),getString(R.string.Le_Méridien_address),getString(R.string.Le_Méridien_contact)));
         Intent i= new Intent(MainActivity.this,ListActivity.class);
         Bundle bundle = new Bundle();
         bundle.putParcelableArrayList("data",List);
@@ -33,7 +33,12 @@ public class MainActivity extends AppCompatActivity {
     public void openRestaurantActivity(View view)
     {
         ArrayList<TouristSpot> List = new ArrayList<>();
-        List.add(new TouristSpot(getString(R.string.ITC_hotel), getString(R.string.ITC_short_desc), R.drawable.sample,getString(R.string.ITC_desc),getString(R.string.ITC_address),getString(R.string.ITC_contact)));
+        List.add(new TouristSpot(getString(R.string.RJ14_rest), getString(R.string.RJ14_short_desc), R.drawable.rj14,getString(R.string.RJ14_desc),getString(R.string.RJ14_address),getString(R.string.RJ14_contact)));
+        List.add(new TouristSpot(getString(R.string.The_Rajput_Room_rest), getString(R.string.The_Rajput_Room_short_desc), R.drawable.rajputroom,getString(R.string.The_Rajput_Room_desc),getString(R.string.The_Rajput_Room_address),getString(R.string.The_Rajput_Room_contact)));
+        List.add(new TouristSpot(getString(R.string.Jaipur_Adda_rest), getString(R.string.Jaipur_Adda_short_desc), R.drawable.jaipuradda,getString(R.string.Jaipur_Adda_desc),getString(R.string.Jaipur_Adda_address),getString(R.string.Jaipur_Adda_contact)));
+        List.add(new TouristSpot(getString(R.string.Zolocrust_rest), getString(R.string.Zolocrust_short_desc), R.drawable.zolocrust,getString(R.string.Zolocrust_desc),getString(R.string.Zolocrust_address),getString(R.string.Zolocrust_contact)));
+        List.add(new TouristSpot(getString(R.string.TapriCentral_rest), getString(R.string.TapriCentral_short_desc), R.drawable.tapri,getString(R.string.TapriCentral_desc),getString(R.string.TapriCentral_address),getString(R.string.TapriCentral_contact)));
+        List.add(new TouristSpot(getString(R.string.Chao_Chinese_rest), getString(R.string.Chao_Chinese_short_desc), R.drawable.chao,getString(R.string.Chao_Chinese_desc),getString(R.string.Chao_Chinese_address),getString(R.string.Chao_Chinese_contact)));
         Intent i= new Intent(MainActivity.this,ListActivity.class);
         Bundle bundle = new Bundle();
         bundle.putParcelableArrayList("data",List);
@@ -43,12 +48,11 @@ public class MainActivity extends AppCompatActivity {
     public void openSpotActivity(View view)
     {
         ArrayList<TouristSpot> List = new ArrayList<>();
-        List.add(new TouristSpot(getString(R.string.Amber_Fort_spot), getString(R.string.Amber_Fort_short_desc), R.drawable.sample,getString(R.string.Amber_Fort_desc),getString(R.string.Amber_Fort_address),getString(R.string.Amber_Fort_contact)));
-        List.add(new TouristSpot(getString(R.string.Birla_Mandir_spot), getString(R.string.Birla_Mandir_short_desc), R.drawable.sample,getString(R.string.Birla_Mandir_desc),getString(R.string.Birla_Mandir_address),getString(R.string.Birla_Mandir_contact)));
-        List.add(new TouristSpot(getString(R.string.Hawa_Mahal_spot), getString(R.string.Hawa_Mahal_short_desc), R.drawable.sample,getString(R.string.Hawa_Mahal_desc),getString(R.string.Hawa_Mahal_address),getString(R.string.Hawa_Mahal_contact)));
-        List.add(new TouristSpot(getString(R.string.Jantar_Mantar_spot), getString(R.string.Jantar_Mantar_short_desc), R.drawable.sample,getString(R.string.Jantar_Mantar_desc),getString(R.string.Jantar_Mantar_address),getString(R.string.Jantar_Mantar_contact)));
+        List.add(new TouristSpot(getString(R.string.Birla_Mandir_spot), getString(R.string.Birla_Mandir_short_desc), R.drawable.birlamandir,getString(R.string.Birla_Mandir_desc),getString(R.string.Birla_Mandir_address),getString(R.string.Birla_Mandir_contact)));
+        List.add(new TouristSpot(getString(R.string.Hawa_Mahal_spot), getString(R.string.Hawa_Mahal_short_desc), R.drawable.hawamahal,getString(R.string.Hawa_Mahal_desc),getString(R.string.Hawa_Mahal_address),getString(R.string.Hawa_Mahal_contact)));
+        List.add(new TouristSpot(getString(R.string.Jantar_Mantar_spot), getString(R.string.Jantar_Mantar_short_desc), R.drawable.jantarmantar,getString(R.string.Jantar_Mantar_desc),getString(R.string.Jantar_Mantar_address),getString(R.string.Jantar_Mantar_contact)));
         List.add(new TouristSpot(getString(R.string.Jal_Mahal_spot), getString(R.string.Jal_Mahal_short_desc), R.drawable.sample,getString(R.string.Jal_Mahal_desc),getString(R.string.Jal_Mahal_address),getString(R.string.Jal_Mahal_contact)));
-        List.add(new TouristSpot(getString(R.string.Galta_Ji_spot), getString(R.string.Galta_Ji_short_desc), R.drawable.sample,getString(R.string.Galta_Ji_desc),getString(R.string.Galta_Ji_address),getString(R.string.Galta_Ji_contact)));
+        List.add(new TouristSpot(getString(R.string.Galta_Ji_spot), getString(R.string.Galta_Ji_short_desc), R.drawable.galtaji,getString(R.string.Galta_Ji_desc),getString(R.string.Galta_Ji_address),getString(R.string.Galta_Ji_contact)));
         Intent i= new Intent(MainActivity.this,ListActivity.class);
         Bundle bundle = new Bundle();
         bundle.putParcelableArrayList("data",List);
@@ -58,7 +62,9 @@ public class MainActivity extends AppCompatActivity {
     public void openTransportActivity(View view)
     {
         ArrayList<TouristSpot> List = new ArrayList<>();
-        List.add(new TouristSpot(getString(R.string.ITC_hotel), getString(R.string.ITC_short_desc), R.drawable.sample,getString(R.string.ITC_desc),getString(R.string.ITC_address),getString(R.string.ITC_contact)));
+        List.add(new TouristSpot(getString(R.string.metro), getString(R.string.metro_short_desc), R.drawable.download,getString(R.string.metro_desc),getString(R.string.metro_address),getString(R.string.metro_contact)));
+        List.add(new TouristSpot(getString(R.string.LowFloor_bus), getString(R.string.LowFloor_short_desc), R.drawable.lowfloor,getString(R.string.metro_desc),getString(R.string.metro_address),getString(R.string.metro_contact)));
+        List.add(new TouristSpot(getString(R.string.Palace_On_Wheels), getString(R.string.Palace_On_Wheels_short_desc), R.drawable.palaceonwheels,getString(R.string.Palace_On_Wheels_desc),getString(R.string.Palace_On_Wheels_address),getString(R.string.Palace_On_Wheels_contact)));
         Intent i= new Intent(MainActivity.this,ListActivity.class);
         Bundle bundle = new Bundle();
         bundle.putParcelableArrayList("data",List);
@@ -68,7 +74,9 @@ public class MainActivity extends AppCompatActivity {
     public void openShoppingActivity(View view)
     {
         ArrayList<TouristSpot> List = new ArrayList<>();
-        List.add(new TouristSpot(getString(R.string.ITC_hotel), getString(R.string.ITC_short_desc), R.drawable.sample,getString(R.string.ITC_desc),getString(R.string.ITC_address),getString(R.string.ITC_contact)));
+        List.add(new TouristSpot(getString(R.string.Chandpole_Bazaar_shop), getString(R.string.Chandpole_Bazaar_short_desc), R.drawable.chandpol,getString(R.string.Chandpole_Bazaar_desc),getString(R.string.Chandpole_Bazaar_address),getString(R.string.Chandpole_Bazaar_contact)));
+        List.add(new TouristSpot(getString(R.string.Tripolia_Bazaar_shop), getString(R.string.Tripolia_Bazaar_short_desc), R.drawable.tripolia,getString(R.string.Tripolia_Bazaar_desc),getString(R.string.Tripolia_Bazaar_address),getString(R.string.Tripolia_Bazaar_contact)));
+        List.add(new TouristSpot(getString(R.string.Johari_Bazaar_shop), getString(R.string.Johari_Bazaar_short_desc), R.drawable.johari,getString(R.string.Johari_Bazaar_desc),getString(R.string.Johari_Bazaar_address),getString(R.string.Johari_Bazaar_contact)));
         Intent i= new Intent(MainActivity.this,ListActivity.class);
         Bundle bundle = new Bundle();
         bundle.putParcelableArrayList("data",List);
